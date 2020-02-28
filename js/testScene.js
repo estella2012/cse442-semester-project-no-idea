a2dd8bf49cb35a3b90cbb8b14012b03a6f814842
estella2012 <jinyitao@buffalo.edu>
2020-02-28 01:14:30 -0500

    Added music to main menu Testing on game scene movement

--- js/testScene.js
+++ js/testScene.js
@@ -1,37 +1,3 @@
-
-    var SceneGame = new Phaser.Class({
-
-    	Extends: Phaser.Scene,
-
-    	initialize:
-
-    	function SceneGame ()
-    {
-        Phaser.Scene.call(this, { key: 'sceneGame' });
-    },
-
-	    preload: function ()a
-    var SceneGame = new Phaser.Class({
-
-    	Extends: Phaser.Scene,
-
-    	initialize:
-
-    	function SceneGame ()
-    {
-        Phaser.Scene.call(this, { key: 'sceneGame' });
-    },
-
-	    preload: function ()
-	    {
-	        this.load.image('hello', 'assets/testScene.png');
-	    },
-
-	    create: function ()
-	    {
-	        this.add.image(400, 300, 'hello').setScale(0.5);
-	    }
-	});
     /*
     *Music control
     */
@@ -71,62 +37,18 @@
 	        switchON.on('pointerdown', function(){
 
 	        	switchON.setVisible(0);
-	        	switchOFF.setVisible(1)
+				switchOFF.setVisible(1)
+				//theme.setMute(false);
 
 	        });
 
 	        switchOFF.on('pointerdown', function(){
 
 	        	switchON.setVisible(1);
-	        	switchOFF.setVisible(0)
-
+				switchOFF.setVisible(0)
+				//theme.setMute(true);
 	        });
 	        
-	        close.once('pointerdown', function () {
-
-            this.scene.start('sceneA');
-
-        }, this);
-
-	    }
-	});
-
-
-
-	    {
-	        this.load.image('hello', 'assets/testScene.png');
-	    },
-
-	    create: function ()
-	    {
-	        this.add.image(400, 300, 'hello').setScale(0.5);
-	    }
-	});
-
-	var SceneMusic = new Phaser.Class({
-
-    	Extends: Phaser.Scene,
-
-    	initialize:
-
-    	function SceneMusic ()
-    {
-        Phaser.Scene.call(this, { key: 'sceneMusic' });
-
-    },
-
-	    preload: function ()
-	    {
-	        this.load.image('close', 'assets/close.png');
-	        this.load.image('hello', 'assets/testScene.png');
-	    },
-
-	    create: function ()
-	    {
-	    	this.add.image(400, 300, 'hello').setScale(0.4);
-	        
-	        var close = this.add.image(60, 470, 'close').setScale(0.15).setInteractive();
-
 	        close.once('pointerdown', function () {
 
             this.scene.start('sceneA');
