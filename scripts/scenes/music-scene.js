410bf208e138c8dc6f1963f7395c7fe5a16ba50b
emerycomstock <emerycomstock@gmail.com>
2020-04-15 14:59:14 -0400

    Issues #39 and #106: Added new UI sprites for volume control at main menu and also implemented volume increase and decrease functionality at music settings screen

--- scripts/scenes/music-scene.js
+++ scripts/scenes/music-scene.js
@@ -4,7 +4,9 @@ export class MusicScene extends Phaser.Scene {
     }
 
     preload() {
-	    this.load.image('close', 'assets/main-menu/close.png');
+        this.load.image('close', 'assets/main-menu/close.png');
+        this.load.image('increase', 'assets/main-menu/increase.png');
+        this.load.image('decrease', 'assets/main-menu/decrease.png');
 	    this.load.image('bg', 'assets/backgrounds/gray.png');
     }
 
@@ -14,6 +16,11 @@ export class MusicScene extends Phaser.Scene {
 
         var close = this.add.image(60, 470, 'close').setScale(0.15).setInteractive();
         var onOff = this.add.text(308, 210, 'Mute: ON', {font: '32px Arial'}).setInteractive();
+
+        var volRate = 0.05;
+        var volumeText = this.add.text(308, 240, 'Volume: ' + Math.round(this.sound.volume * 100), {font: '32px Arial'});
+        var decVolume = this.add.image(280, 258, 'decrease').setScale(0.1).setInteractive();
+        var incVolume = this.add.image(516, 258, 'increase').setScale(0.1).setInteractive();
 
         if (!this.sound.mute) {
             onOff.setText('Mute: OFF');
@@ -27,7 +34,29 @@ export class MusicScene extends Phaser.Scene {
                 onOff.setText('Mute: OFF');
             }
         }, this);
-	        
+            
+        incVolume.on('pointerdown', function() {
+            var newVol = this.sound.volume + volRate
+
+            if (newVol > 1) {
+                newVol = 1;
+            }
+
+            this.sound.volume = newVol;
+            volumeText.setText('Volume: ' + Math.round(newVol * 100));
+        }, this);
+
+        decVolume.on('pointerdown', function() {
+            var newVol = this.sound.volume - volRate
+
+            if (newVol < 0) {
+                newVol = 0;
+            }
+
+            this.sound.volume = newVol;
+            volumeText.setText('Volume: ' + Math.round(newVol * 100));
+        }, this);
+
 	    close.on('pointerdown', function() {
             this.scene.stop()
             this.scene.wake('MainMenuScene');
