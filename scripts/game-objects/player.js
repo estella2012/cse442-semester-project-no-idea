6d1783617e6eda310df25681b00b3a5853f5d19a
samathir <43017727+samathir@users.noreply.github.com>
2020-05-07 22:05:35 -0400

    #101 animation fixed
    - the player has new sprite and animation

--- scripts/game-objects/player.js
+++ scripts/game-objects/player.js
@@ -5,24 +5,24 @@ export class Player extends Phaser.GameObjects.Sprite {
         this.setTexture('player');
     }
 
-    preUpdate() {
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
         if (this.cursors.left.isDown) {
             this.body.setVelocityX(-160);
             this.anims.play('player-left', true);
         } else if (this.cursors.right.isDown) {
             this.body.setVelocityX(160);
             this.anims.play('player-right', true);
-        } else {
-            this.body.setVelocityX(0);
-            this.anims.play('player-turn');
-        }
-
-        if (this.cursors.up.isDown) {
+        } else if (this.cursors.up.isDown) {
+            this.anims.play('player-up', true);
             this.body.setVelocityY(-160);
         } else if (this.cursors.down.isDown) {
+            this.anims.play('player-down', true);
             this.body.setVelocityY(160);
         } else {
             this.body.setVelocityY(0);
+            this.body.setVelocityX(0);
+            this.anims.play('player-turn');
         }
     }
 }
