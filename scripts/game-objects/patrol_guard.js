7a24a9072deadbe98247c827d2cb1c3d6a069627
samathir <43017727+samathir@users.noreply.github.com>
2020-05-07 22:07:35 -0400

    #101 animation fix
    -now guard can move with correct animation

--- scripts/game-objects/patrol_guard.js
+++ scripts/game-objects/patrol_guard.js
@@ -6,15 +6,37 @@ export class PatrolGuard extends Phaser.GameObjects.PathFollower {
         path.lineTo(x, y + h);
         path.lineTo(x, y);
 
+
+
         super(scene, path, 0, 0, 'patrol_guard');
 
-        this.startFollow({
+       this.startFollow({
             positionOnPath: true,
             duration: 8000,
             repeat: -1,
             rotateToPath: false,
             verticalAdjust: true
         });
+
+
+
+    }
+ preUpdate(time, delta){
+        super.preUpdate(time, delta);
+
+
+     var Clamp = Phaser.Math.Clamp;
+
+     var t = this.pathTween.getValue();
+     var p1 = this.path.getPoint(Clamp(t - 1e-6, 0, 1));
+     var p2 = this.path.getPoint(Clamp(t + 1e-6, 0, 1));
+     var dir = p2.clone().subtract(p1);
+
+     if (dir.x > 0) this.anims.play("guard-right", true);
+     else if (dir.x < 0) this.anims.play("guard-left", true);
+     else if (dir.y > 0) this.anims.play("guard-down", true);
+     else if (dir.y < 0) this.anims.play("guard-up", true);
+     else this.anims.stop();
     }
 
     gameOver (player, guard)
